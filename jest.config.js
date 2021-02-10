b6d2087eaadb5708ab59761f54e5b56bb3a7f9b4
Harbs <harbsprog@gmail.com>
2021-02-09 21:44:58 -0300

    build: added dependencies for project run

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    clearMocks: true,
+    preset: 'ts-jest',
+    roots: ['<rootDir>'],
+    testEnvironment: 'node',
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)',
+    ],
+    testPathIgnorePatterns: ['/node_modules/', '/src/__tests__/mocks'],
+};
